a781115a1f9346631bb750f11b60397e9f0e2e60
Rashmi Sharmila <108237108+rashmisharmila@users.noreply.github.com>
2023-10-18 19:45:32 +0530

    updated with ajax in indexController.js

--- assets/js/AdminDashboard.js
+++ assets/js/AdminDashboard.js
@@ -65,11 +65,63 @@ $.ajax({
 })
 
 //User
-$("UsersCount").val("0");
+$("#UsersCount").val("0");
 $.ajax({
-
+    url:adminDashboardBaseUrl+"userCount",
+    method:"GET",
+    contentType:"application/json",
+    dataType:"json",
+    success:function (res){
+        let num=res.count
+        $("#UsersCount").text(num);
+    },
+    error:function (error){
+        console.log(error)
+    }
 })
 
 //Guide
+$("#GuideCount").val("0");
+$.ajax({
+    url:adminDashboardBaseUrl+"GuiderCount",
+    method:"GET",
+    contentType:"application/json",
+    dataType:"json",
+    success:function (res){
+        let num=res.count
+        $("#GuideCount").text(num);
+    },
+    error:function (error){
+        console.log(error)
+    }
+})
 //Customer
+$("#customerCount").val("0");
+$.ajax({
+    url:adminDashboardBaseUrl+"CustomerCount",
+    method:"GET",
+    contentType:"application/json",
+    dataType:"json",
+    success:function (res){
+        let num=res.count
+        $("#customerCount").text(num);
+    },
+    error:function (error){
+        console.log(error)
+    }
+})
 //Today Booking
+$("#tBookingCount").val("0");
+$.ajax({
+    url:adminDashboardBaseUrl+"CustomerCount",
+    method:"GET",
+    contentType:"application/json",
+    dataType:"json",
+    success:function (res){
+        let num=res.count
+        $("#tBookingCount").text(num);
+    },
+    error:function (error){
+        console.log(error)
+    }
+})
